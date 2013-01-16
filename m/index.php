dce17a359b2f1cab33ac78dcebbad9452d6480e4
Magic Magister <magic1995221@web.de>
2013-01-17 00:08:27 +0100

    Add Site for PhoneGap

---
+++ m/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Unbenanntes Dokument</title>
+<style>
+	
+body{
+	margin:0;
+	background-image:url("../assets/images/backgrounds/main-bg.png");
+	font-family:"Helvetica-Neue","Arial",sans-serif !important;
+	overflow-x:hidden;
+	}
+.clearer{
+	clear:both;
+	}
+	
+.main-wrapper{
+	position:relative;
+	top:0;
+	left:0;
+	width:100%;
+	}
+.top-line{
+	position:relative;
+	top:0;
+	left:0;
+	width:100%;
+	height:30px;
+	background-color:#444;
+	}
+.menu-container{
+	position:relative;
+	border-bottom:1px solid #666;
+    box-shadow:0 0 3px rgba(0, 0, 0, 0.4);
+    height:9em;
+    width:100%;
+	/*background: linear-gradient(#6FACD5, #497BAE) repeat scroll 0 0 #5E87B0;*/
+	background-image:url("../assets/images/backgrounds/noise.png"), -webkit-gradient(linear,left top,left bottom,from(#95dae7),to(#438996));
+	background-image:url("../assets/images/backgrounds/noise.png"), -webkit-linear-gradient(#95dae7,#438996);
+	background-image:url("../assets/images/backgrounds/noise.png"), -moz-linear-gradient(#95dae7,#438996);
+	background-image:url("../assets/images/backgrounds/noise.png"), -ms-linear-gradient(#95dae7,#438996);
+	background-image:url("../assets/images/backgrounds/noise.png"), -o-linear-gradient(#95dae7,#438996);
+	background-image:url("../assets/images/backgrounds/noise.png"), linear-gradient(#95dae7,#438996);
+	background-color:#65A9B6;
+	z-index:5;
+	}
+.top-tabbar{
+	position:relative;
+	width:100%;
+	height:5.5em;
+	background-image:-webkit-gradient(linear,left top,left bottom,from(#F1F1F1),to(#D8D8D8));
+	background-image:-webkit-linear-gradient(#F1F1F1,#D8D8D8);
+	background-image:-moz-linear-gradient(#F1F1F1,#D8D8D8);
+	background-image:-ms-linear-gradient(#F1F1F1,#D8D8D8);
+	background-image:-o-linear-gradient(#F1F1F1,#D8D8D8);
+	background-image:linear-gradient(#F1F1F1,#D8D8D8);
+	background-color:#EEE;
+	border-bottom:1px solid #B5B5B5;
+	box-shadow:0 0 3px rgba(0, 0, 0, 0.2);
+	z-index:4;
+	}
+.main-container{
+	position:relative;
+    min-height:600px;
+    width:100%;
+	padding:20px;
+	box-sizing:border-box;
+    -moz-box-sizing:border-box; /* Firefox */
+    -webkit-box-sizing:border-box; /* Safari */
+	z-index:3;
+	}
+._input-text{
+	position:relative;
+    margin:0 auto;
+	padding:8px 35px 8px 30px;
+	background: none repeat scroll 0 0 rgba(255, 255, 255, 0.8);
+    border: 1px solid #DBDBDB;
+    border-radius: 5px 5px 5px 5px;
+	overflow:hidden;
+	
+	box-sizing:border-box;
+    -moz-box-sizing:border-box; /* Firefox */
+    -webkit-box-sizing:border-box; /* Safari */
+	}
+input{
+	position:relative;
+    margin:0 auto;
+    width:100%;
+	border:none;
+	background:none;
+	color:#444;
+	padding:0;
+	font-size:3em;
+	
+	box-sizing:border-box;
+    -moz-box-sizing:border-box; /* Firefox */
+    -webkit-box-sizing:border-box; /* Safari */
+	}
+.search-logo{
+	position:absolute;
+    left:5px;
+	top:0;
+    width:25px;
+	height:100%;
+	border:none;	
+	background-image:url("../assets/images/symbols/icons-white.png");
+	background-position:-612px 50%;
+	background-repeat:no-repeat;
+	}
+.info_first{
+	position:absolute;
+	height:200px;
+	width:80%;
+	margin: 50px 0;
+	left:10%;
+	border-radius:20px;
+	background:url(../assets/images/backgrounds/bg_de.png);
+	border: 2px solid #999;
+	}
+</style>
+</head>
+
+<body>
+
+<!--<div class="main-wrapper">
+
+    <div class="top-line"></div>
+    <div class="menu-container"></div>
+    <div class="top-tabbar"></div>
+    
+    <div class="main-container">
+        <div class="_input-text" style="width:500px;">
+            <input type="text"/>
+            <div class="search-logo"></div>
+        </div>
+    </div>
+    
+    <div class="clearer"></div>
+
+</div>-->
+
+<div class="info_first" >
+
+</div>
+ 
+
+</body>
+</html>
